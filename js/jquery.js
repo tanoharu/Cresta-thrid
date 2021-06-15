8ae5ee40c8229b7fa94e791056d46ec3faf852e4
tanoharu <78711749+tanoharu@users.noreply.github.com>
2021-06-15 15:04:14 +0900

    Responsive support for the 'contact' page is complete.

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,13 @@
 $(function () {
 
-	$('.c-button--hamburger').on('click', function () {
-		$('.c-button--hamburger').toggleClass('close');
+	// $('.c-button--hamburger').on('click', function () {
+	// 	$('.c-button--hamburger').toggleClass('close');
+	// 	$('.p-hamburger-menu').fadeToggle('500');
+	// 	$('body').toggleClass('nonscroll');
+	// });
+
+	$('.c-button--hamburger, .c-button--hamburger__contact').on('click', function () {
+		$('.c-button--hamburger, .c-button--hamburger__contact').toggleClass('close');
 		$('.p-hamburger-menu').fadeToggle('500');
 		$('body').toggleClass('nonscroll');
 	});
