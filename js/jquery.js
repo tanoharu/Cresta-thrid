a7a36dbba387077dc3de9f3592c6dd3af8469089
tanoharu <78711749+tanoharu@users.noreply.github.com>
2021-06-15 19:58:33 +0900

    Inherit style using @extend. 'contents.scss' is subdivided into each file and deleted.

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,5 @@
 $(function () {
 
-	// $('.c-button--hamburger').on('click', function () {
-	// 	$('.c-button--hamburger').toggleClass('close');
-	// 	$('.p-hamburger-menu').fadeToggle('500');
-	// 	$('body').toggleClass('nonscroll');
-	// });
-
 	$('.c-button--hamburger, .c-button--hamburger__contact').on('click', function () {
 		$('.c-button--hamburger, .c-button--hamburger__contact').toggleClass('close');
 		$('.p-hamburger-menu').fadeToggle('500');
