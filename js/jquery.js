f9714d8a5b1e2928b949f4e9a7c14db9917c337a
tanoharu <78711749+tanoharu@users.noreply.github.com>
2021-06-17 09:53:37 +0900

    Fixed responsive

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,7 @@
 $(function () {
 
-	$('.c-button--hamburger, .c-button--hamburger__contact').on('click', function () {
-		$('.c-button--hamburger, .c-button--hamburger__contact').toggleClass('close');
+	$('.c-button--hamburger').on('click', function () {
+		$('.c-button--hamburger').toggleClass('close');
 		$('.p-hamburger-menu').fadeToggle('500');
 		$('body').toggleClass('nonscroll');
 	});
